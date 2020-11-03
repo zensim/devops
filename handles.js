95cd831608768fd72a5df44bfc16d6cc17bdf6db
zensim <dec200069@yahoo.com>
2020-11-03 16:37:23 -0600

    Add files via upload

--- handles.js
+++ handles.js
@@ -1,12 +1,11 @@
 
 exports.endpoint = async function(event) {
-    const current_time = Date.now().current_time;
-    //console.log("request:", JSON.stringify(event, undefined, 2));
+    console.log("request:", JSON.stringify(event, undefined, 2));
     return {
       statusCode: 200,
-      //headers: { "Content-Type": "text/plain" },
+      headers: { "Content-Type": "text/plain" },
       body: {
-            "message": "Hello, the current time is " + String(current_time)
+            "message": "Hello, from serverless yaml lambda"
         }
     };
   };
