55130ca84e36120ce32e30d4d5fc428dbe5cc51a
zensim <dec200069@yahoo.com>
2020-11-02 22:24:25 -0600

    Add files via upload

---
+++ handles.js
@@ -0,0 +1,12 @@
+
+exports.handler = async function(event) {
+    const current_time = Date.now().current_time;
+    //console.log("request:", JSON.stringify(event, undefined, 2));
+    return {
+      statusCode: 200,
+      //headers: { "Content-Type": "text/plain" },
+      body: {
+            "message": "Hello, the current time is " + String(current_time)
+        }
+    };
+  };
